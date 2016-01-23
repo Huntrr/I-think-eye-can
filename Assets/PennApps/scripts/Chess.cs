b266badce9ef1869d2d4d78a63ace9f5eb146cb9
Hunter Lightman <git.huntrr@gmail.com>
2016-01-23 10:20:50 -0500

    jskdfaljsdlkf

--- Assets/PennApps/scripts/Chess.cs
+++ Assets/PennApps/scripts/Chess.cs
@@ -3,13 +3,29 @@ using System.Collections;
 
 public class Chess : MonoBehaviour {
 
+	public GameObject black_pawn;
+	public GameObject black_rook;
+	public GameObject black_bishop;
+	public GameObject black_knight;
+	public GameObject black_king;
+	public GameObject black_queen;
+
+	public GameObject white_pawn;
+	public GameObject white_rook;
+	public GameObject white_bishop;
+	public GameObject white_knight;
+	public GameObject white_king;
+	public GameObject white_queen;
+
 	// Use this for initialization
 	void Start () {
-	
+		print ("AWDWD");
+		GameObject o = Instantiate (Resources.Load("Chess_Rock_B1"), new Vector3 (0, 0, 0), Quaternion.identity) as GameObject;
+		o.transform.parent = transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
---
+++ Assets/PennApps/scripts/Loader.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class Loader : MonoBehaviour, ITrackableEventHandler {
+	private TrackableBehaviour mTrackableBehaviour;
+	private Transform currentModel;
+
+	public Transform chess;
+	public Transform skull;
+
+	void Start () {
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		if (mTrackableBehaviour) {
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+	}
+
+	void Update () { }
+
+	public void OnTrackableStateChanged(
+		TrackableBehaviour.Status previousStatus,
+		TrackableBehaviour.Status newStatus) { 
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+			newStatus == TrackableBehaviour.Status.TRACKED ||
+			newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+		{
+			OnTrackingFound();
+		}
+	} 
+
+	private void OnTrackingFound() {
+		if (myModelPrefab != null)
+		{
+			Transform myModelTrf = GameObject.Instantiate(myModelPrefab) as Transform;
+			myModelTrf.parent = mTrackableBehaviour.transform;
+			myModelTrf.localPosition = new Vector3(0f, 0f, 0f);
+			myModelTrf.localRotation = Quaternion.identity;
+			myModelTrf.localScale = new Vector3(4f, 4f, 4f);
+			myModelTrf.gameObject.active = true;
+		}
+	}
+}
